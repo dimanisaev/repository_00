52f1f9495fa0f1a3d1b13ffe0f69d4f66f642eb0
Dmitry Isaev <dmisaev@icloud.com>
2025-08-03 09:12:38 +0300

    add a function to calculate factorial

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("My first repository")
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))  # Output: 120
